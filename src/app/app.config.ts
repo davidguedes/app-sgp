3072063527972f5ec2d3cf97cb9c102d9c1b4988
David <d2vid.guedes@gmail.com>
2026-02-18 11:16:02 -0300

    fix: ultimos ajustes pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -6,7 +6,8 @@ import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeuix/themes/lara';
 import { httpAuthInterceptor } from './core/interceptors/auth.interceptor';
-import pt from 'primelocale/pt.json'; // Importe o JSON de tradução
+import pt from 'primelocale/pt.json';
+import { provideServiceWorker } from '@angular/service-worker'; // Importe o JSON de tradução
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,6 +23,9 @@ export const appConfig: ApplicationConfig = {
         preset: Lara
       },
       translation: pt.pt
-    }),
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 };
